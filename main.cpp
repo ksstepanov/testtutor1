0c782c64df94abfd2291c6c2a5653f99a0870199
Konstantin_Stepanov <ksstepanov92@gmail.com>
2012-10-19 22:41:02 +0400

    hello2 addedi

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@ int main(void)
 {
 	printf("Hello!\n");
 	printf("constant from _constants.h_ is: %d\n",main_const);
+	printf("Hello_2!\n");
+	printf("this branch is hello_add. it is parallel to branch calc_inc.\n");
 	getchar();
 	return(1);
 }
