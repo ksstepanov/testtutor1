ecdd6e09addcae63bed4367b7f8203d82344332a
Konstantin_Stepanov <ksstepanov92@gmail.com>
2012-10-19 18:41:15 +0400

    code Hello + constant written and work

--- constants.h
+++ constants.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-printf("yohoho!\n");
-printf("yahaha!\n");
+//printf("yohoho!\n");
+//printf("yahaha!\n");
+const int main_const = 1000;
--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-void main(void)
+#include "constants.h"
+int main(void)
 {
-printf("Hello!\n");
+	printf("Hello!\n");
+	printf("constant from _constants.h_ is: %d\n",main_const);
+	getchar();
+	return(1);
 }
